01e1f99d1bc883b543942e5ed3c5d2542381dbac
jenniferzoey <jenniferzoey@gmail.com>
2019-07-16 11:11:26 -0400

    Added first name and last name to user registration and sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { User.new(first_name: "Ashley", last_name: "Hampson", email: "Ashley.Hampson@gmail.com", password: "password") }
+
+  describe "first_name" do
+  it "should return the user's first name" do
+    expect(user.first_name).to eq("Ashley")
+    end
+  end
+
+  describe "last_name" do
+  it "should return the user's last name" do
+    expect(user.last_name).to eq("Hampson")
+    end
+  end
+
+  describe "email" do
+  it "should return the user's email" do
+    expect(user.email).to eq("Ashley.Hampson@gmail.com")
+    end
+  end
+
+  describe "password" do
+  it "should return the user's password" do
+    expect(user.password.length).to be >= 6
+    end
+  end
 end
