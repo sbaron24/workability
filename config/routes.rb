32be193bebc0fa0d7d0281008e48a63dddce077c
JAY KIM <JAYHKIM23@GMAIL.COM>
2019-07-17 09:49:32 -0400

    index_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'places#index'
   devise_for :users
-  get "/places", to: "places#index"
+  
   resources :places, only: [:index]
 end
--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Place, type: :model do
-
-describe 'validations' do
-  it { should validate_presence_of(:name)}
-  it { should validate_presence_of(:place_type)}
-  it { should validate_presence_of(:location)}
-  it { should validate_presence_of(:description)}
+  describe 'validations' do
+    it { should validate_presence_of(:name)}
+    it { should validate_presence_of(:place_type)}
+    it { should validate_presence_of(:location)}
+    it { should validate_presence_of(:description)}
+  end
 end
