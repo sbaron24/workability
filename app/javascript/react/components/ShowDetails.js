18a77a8d30e93a80356a2046ffe32c3b6beb214c
JAY KIM <JAYHKIM23@GMAIL.COM>
2019-07-25 11:34:26 -0400

    Working on places show page details

--- app/javascript/react/components/ShowDetails.js
+++ app/javascript/react/components/ShowDetails.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 const ShowDetails = (props) => {
-
+  debugger
   return (
     <div>
-      <p> Name: {props.place.name} </p>
-      <p> Neighborhood: {props.place.neighborhood} </p>
-      <p> Type: {props.place.place_type} </p>
+      <h1>{props.place.name}</h1>
+      <p>{props.place.address} {props.place.city}, {props.place.state} {props.place.zip}</p>
+      <p>{props.place.neighborhood}</p>
+      <p>{props.place.place_type}</p>
       <p> Descripton: {props.place.description} </p>
     </div>
   )
--- app/serializers/place_serializer.rb
+++ app/serializers/place_serializer.rb
@@ -1,3 +1,4 @@
 class PlaceSerializer < ActiveModel::Serializer
-  attributes :id, :name, :place_type, :neighborhood, :description
+  attributes :id, :name, :place_type, :neighborhood, :description, :wifi,
+  :food, :outdoor_seating, :standing_options, :address, :city, :state, :zip
 end
